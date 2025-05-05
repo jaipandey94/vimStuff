ac7a337a60eab2a02c2bb03bc1d1481fd736b396
Jai Pandey <jai.pandey94@live.com>
2025-05-05 14:40:28 -0700

    More quality of life changes

--- nvimConfig/init.lua
+++ nvimConfig/init.lua
@@ -14,6 +14,8 @@ vim.wo.relativenumber = true
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 -- vim.cmd("filetype indent plugin off")
+
+-- File navigation
 vim.cmd.nnoremap("<C-d>", "<C-d>zz")
 vim.cmd.nnoremap("<C-u>", "<C-u>zz")
 vim.cmd.nnoremap("<C-z>", "u")
@@ -24,14 +26,16 @@ vim.keymap.set("n", "G", "<Nop>")
 vim.keymap.set("n", "gg", "<Nop>")
 vim.keymap.set("n", "<C-y>", "<C-r>")
 vim.keymap.set("n", " ", "<Nop>")
--- vim.keymap.set("n", "H", "h")
--- vim.keymap.set("n", "J", "j")
--- vim.keymap.set("n", "K", "k")
--- vim.keymap.set("n", "L", "L")
--- vim.keymap.set("v", "H", "h")
--- vim.keymap.set("v", "J", "j")
--- vim.keymap.set("v", "K", "k")
--- vim.keymap.set("v", "L", "L")
+vim.keymap.set("n", "H", "h")
+vim.keymap.set("n", "J", "j")
+vim.keymap.set("n", "K", "k")
+vim.keymap.set("n", "L", "L")
+vim.keymap.set("v", "H", "h")
+vim.keymap.set("v", "J", "j")
+vim.keymap.set("v", "K", "k")
+vim.keymap.set("v", "L", "L")
+
+--  Fzf binds
 vim.keymap.set("n", "<leader>f", "<CMD>FzfLua files<CR>")
 vim.keymap.set("n", "<leader>F", "<CMD>FzfLua live_grep<CR>")
 vim.keymap.set("n", "<C-f>", "<CMD>FzfLua lgrep_curbuf<CR>")
@@ -40,23 +44,40 @@ vim.keymap.set("v", "<leader>F", "<CMD>FzfLua grep_visual<CR>")
 vim.keymap.set("n", "<leader>R", "<CMD>FzfLua resume<CR>")
 vim.keymap.set("v", "<C-f>", "<CMD>FzfLua lgrep_curbuf<CR>")
 
+-- Writing and Quitting
 vim.keymap.set("n", "<leader>ww", "<CMD>wa<CR>")
 vim.keymap.set("n", "<leader>wf", "<CMD>wa!<CR>")
-vim.keymap.set("n", "<leader>wq", "<CMD>wqa!<CR>")
+vim.keymap.set("n", "<leader>wq", "<CMD>waq!<CR>")
 vim.keymap.set("n", "<leader>qq", "<CMD>q<CR>")
 vim.keymap.set("n", "<leader>qf", "<CMD>q!<CR>")
 
+-- Marks
+vim.keymap.set("n", "<M-h>", "'h")
+vim.keymap.set("n", "<M-j>", "'j")
+vim.keymap.set("n", "<M-k>", "'k")
+vim.keymap.set("n", "<M-l>", "'l")
+
+vim.keymap.set("n", "<M-H>", "<CMD>delmarks h<CR>")
+vim.keymap.set("n", "<M-J>", "<CMD>delmarks j<CR>")
+vim.keymap.set("n", "<M-K>", "<CMD>delmarks k<CR>")
+vim.keymap.set("n", "<M-L>", "<CMD>delmarks l<CR>")
+
+-- Copy/Paste registers
 vim.keymap.set("v", "y", "\"+y")
 vim.keymap.set("v", "yy", "\"+yy")
 vim.keymap.set("v", "Y", "\"+y$")
 vim.keymap.set("v", "dd", "\"+dd")
 vim.keymap.set("v", "D", "\"+D")
 vim.keymap.set("v", "x", "\"+x")
+
+-- Code Formatting
 vim.opt["tabstop"] = 4
 vim.opt["shiftwidth"] = 4
 vim.opt.expandtab = true
 vim.opt["listchars"] = "eol:$,tab:>-,trail:~,extends:>,precedes:<"
 vim.opt.list = true
+
+-- Ease of life rebinds
 vim.cmd([[
         cnoreabbrev E Explore
         cnoreabbrev e Explore
@@ -94,3 +115,4 @@ vim.opt.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
 vim.opt.shellpipe = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
 vim.opt.shellquote = ''
 vim.opt.shellxquote = ''
+
